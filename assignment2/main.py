dcccff3b4a3f28dc295c454d8a69b547ed4bd190
TeoB2003 <bolota.teofil2003@gmail.com>
2024-10-21 12:32:24 +0300

    final

--- assignment2/main.py
+++ assignment2/main.py
@@ -17,59 +17,60 @@ def download_mnist(is_train: bool):
 train_X, train_Y = download_mnist(True)
 test_X, test_Y = download_mnist(False)
 
-def softmax(weights):
-    Z_exp = np.exp(weights)
-    softmax_output = Z_exp / np.sum(Z_exp, axis=1, keepdims=True) #impart fiecare element al lui Z_exp la suma
-    return softmax_output
-
-def cross_entropy_loss(probabilities, labels):
-    log_predictions = np.log(probabilities + 1e-8)
-    weighted_log_predictions = labels * log_predictions
-    return np.mean(np.sum(weighted_log_predictions))
 
 num_features = 784
 num_classes=10
 
-g_weights = np.zeros((num_features,num_classes))
+g_weights2=np.zeros((num_classes, num_features))
 g_biases = np.zeros(num_classes)
 
-train_Y = np.eye(10)[train_Y]
-test_Y = np.eye(10)[test_Y]
 
-learning_rate=0.01
+learning_rate=0.05
 
-def train(batch_X, batch_Y):
-    global g_biases, g_weights
-    for i in range(0,len(batch_X)):
-        class_matrix=np.dot(batch_X, g_weights)+ g_biases
-        probabilities=softmax(class_matrix)
-        loss = cross_entropy_loss(probabilities, batch_Y)
-        gradient_loss = probabilities - batch_Y
-        product = np.dot(batch_X.T, gradient_loss) /100
-        for_bias = np.sum(gradient_loss, axis=0) / 100
-        g_biases -= learning_rate * for_bias
-        g_weights -= learning_rate * product
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
 
-def compute_accuracy(predictions, labels):
-    return np.mean(np.argmax(predictions, axis=1) == np.argmax(labels, axis=1))
+def train(train_x,train_y):
+        learning_rate=0.01
+        for x in range(len(train_x)):
+            for i in range(10):
+                if i==train_y[x]:
+                    target=1
+                else:
+                    target=0
+                classified=np.dot(train_x[x], g_weights2[i])+g_biases[i]
+                output = sigmoid(classified) #fara sigmoid rata e de 70%
+                error = target - output
+                #print(f'{classified} si error {error}')
+                g_weights2[i]+=error*learning_rate*train_x[x]
+                g_biases[i]+=error*learning_rate
 
-def train_all(epochs):
-    for epoch in range(epochs):
+
+def train_all(epoch):
+    for e in range(epoch):
         for i in range(0, len(train_X), 100):
             x_batch = np.array(train_X[i:i + 100])
             y_batch = train_Y[i:i + 100]
-            train(x_batch ,y_batch)
-        train_accuracy = compute_accuracy(softmax(np.dot(train_X, g_weights) + g_biases), train_Y)
-        test_accuracy = compute_accuracy(softmax(np.dot(test_X, g_weights) + g_biases), test_Y)
-        print(f'Epoch {epoch + 1}  - Train Accuracy: {train_accuracy * 100:.2f}% - Test Accuracy: {test_accuracy * 100:.2f}%')
-    final_train_accuracy = compute_accuracy(softmax(np.dot(train_X, g_weights) + g_biases), train_Y)
-    final_test_accuracy = compute_accuracy(softmax(np.dot(test_X, g_weights) + g_biases), test_Y)
-    print(f'Final Test Accuracy: {final_test_accuracy * 100:.2f}%')
+            train(x_batch,y_batch)
+        test(e)
+    #testare
 
+def test(epoch):
+    nr_cases = 0
+    good_cases = 0
+    for i1 in range(len(test_X)):
+        nr_cases += 1
+        maxi = -20
+        prediction = 0
+        for k in range(10):
+            rez = np.dot(test_X[i1], g_weights2[k]) + g_biases[k]
+            if rez > maxi:
+                maxi = rez
+                prediction = k
+        #print(f'Am prezis {prediction} si era {train_Y[i1]}')
+        if prediction == test_Y[i1]:
+            good_cases += 1
+    accuracy = good_cases / nr_cases
+    print(f'In epoca {epoch+1} am acuratete {accuracy*100:.2f}%')
 
-
-
-
-train_all(2)
-
-
+train_all(3)
